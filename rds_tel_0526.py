91a2c2fcee01bd9ccef5c4a5b330e64b30f8603d
landmarco <marc.loeffke@gmail.com>
2026-06-01 11:19:58 -0400

    creates a LaunchAgent to manage deployment of the script

--- rds_tel_0526.py
+++ rds_tel_0526.py
@@ -5,69 +5,62 @@ from unidecode import unidecode
 
 ### Parameters
 
-update_time = 10 # sec
+update_time = 10  # sec
 tn_host = "71.210.6.210"
-# tn_port = 23
 tn_port = 5423
-
-
-### debug
-
-# a = 'Frida Hyvönen - Jesus Was a Cross Maker'
-# b = unidecode(a)
-# print(a + '\n' + b)
-
-
-### Get current playing from XML
-
 link = "https://wxdu.org/plmanager/world/ajaxnowplaying.php"
-f = requests.get(link)
+fallback_text = 'A service of the Duke Union and a host of sweetie volunteers'
+retry_delay = 60  # seconds to wait after any failure before restarting
 
-text_xml = f.text
-text_xml_l = len(text_xml)
-text_rt = unidecode(text_xml[39:(text_xml_l - 20)])
+while True:
+    try:
+        ### Get current playing from XML
 
-print(f.text)
-print(text_rt)
+        f = requests.get(link, timeout=15)
+        text_xml = f.text
+        text_rt = unidecode(text_xml[39:(len(text_xml) - 20)])
 
-
-### Establish Telnet connection
-
-tn = telnetlib.Telnet(tn_host, tn_port)
-tn.set_debuglevel(1000)
-tn.read_until(b"LOGIN:", timeout=30)
-tn.write(("*******\n").encode('ascii'))
-tn.read_until(b"PASSWORD:", timeout=30)
-tn.write(("*******\n").encode('ascii'))
+        print(f.text)
+        print(text_rt)
 
 
-### Update RT
+        ### Establish Telnet connection
 
-if text_rt == '':
-    text_rt = 'A service of the Duke Union and a host of sweetie volunteers'
+        tn = telnetlib.Telnet(tn_host, tn_port)
+        tn.set_debuglevel(1000)
+        tn.read_until(b"LOGIN:", timeout=30)
+        tn.write(("*******\n").encode('ascii'))
+        tn.read_until(b"PASSWORD:", timeout=30)
+        tn.write(("*******\n").encode('ascii'))
 
-tn.write(("RT_TEXT=" + text_rt + "\n").encode('ascii'))
-time.sleep(update_time)
 
+        ### Update RT
 
-### Run Update Loop
+        if text_rt == '':
+            text_rt = fallback_text
 
-i = 0
-
-while i == 0:
-
-    f = requests.get(link)  # Get new artist/song info
-    text_xml = f.text
-    text_xml_l = len(text_xml)
-    text_rt_new = unidecode(text_xml[39:(text_xml_l - 20)])
-
-    if text_rt_new == '':
-        text_rt_new = 'A service of the Duke Union and a host of sweetie volunteers'
-
-    if text_rt_new != text_rt:  # Compare with old info, update if new
-        text_rt = text_rt_new
         tn.write(("RT_TEXT=" + text_rt + "\n").encode('ascii'))
-        time.sleep(30 - update_time)
+        time.sleep(update_time)
 
-    time.sleep(update_time) # Wait until next update check
 
+        ### Run Update Loop
+
+        while True:
+
+            f = requests.get(link, timeout=15)  # Get new artist/song info
+            text_xml = f.text
+            text_rt_new = unidecode(text_xml[39:(len(text_xml) - 20)])
+
+            if text_rt_new == '':
+                text_rt_new = fallback_text
+
+            if text_rt_new != text_rt:  # Compare with old info, update if new
+                text_rt = text_rt_new
+                tn.write(("RT_TEXT=" + text_rt + "\n").encode('ascii'))
+                time.sleep(30 - update_time)
+
+            time.sleep(update_time)  # Wait until next update check
+
+    except Exception as e:
+        print(f"Error: {e}. Restarting in {retry_delay}s...")
+        time.sleep(retry_delay)
